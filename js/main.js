3127571b46e0224f8a60bdfd684c183b3a54d684
ba1tazar4ik <ba1tazar4ik@gmail.com>
2020-02-02 23:44:17 +0300

    исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 // eslint-disable-next-line strict
-var mapPin = document.querySelector('#pin');
+var ADVERTISING_TITLES = ['Прекрасная лочуга для романтиков', 'Квартира с отличным видом', 'Шикарные аппартоменты', 'Велликолепный дом для утонченных натур', 'Команта для комфортного ночлега', 'Комната в спокойном общежитии', 'Уютное гнездышко для молодоженов'];
+var ADVERTISING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ADVERTISING_CHECKS = ['12:00', '13:00', '14:00'];
+var ADVERTISING_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var ADVERTISING_DESCRIPTIONS = ['Подходит как туристам, так и бизнесменам.', 'Жилье полностью укомплектовано и недавно отремонтировано.', 'Есть вся необходимая бытовая техника.', 'Есть посуда и кухонные принадлежности.', 'Удобное рассположение в центре Токио', 'Понорамный вид из окон'];
+var ADVERTISING_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var minLocationY = 130;
+var maxLocationY = 630;
+var maxPriceMultiplier = 100;
+var priceMultiplier = 100;
+var maxRooms = 100;
+var maxGuests = 3;
 var mapPinsBlock = document.querySelector('.map__pins');
 var advertisings = [];
-var advertisingTitles = ['Прекрасная лочуга для романтиков', 'Квартира с отличным видом', 'Шикарные аппартоменты', 'Велликолепный дом для утонченных натур', 'Команта для комфортного ночлега', 'Комната в спокойном общежитии', 'Уютное гнездышко для молодоженов'];
-var advertisingTypes = ['palace', 'flat', 'house', 'bungalo'];
-var advertisingChecks = ['12:00', '13:00', '14:00'];
-var advertisingFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var advertisingDescriptions = ['Подходит как туристам, так и бизнесменам.', 'Жилье полностью укомплектовано и недавно отремонтировано.', 'Есть вся необходимая бытовая техника.', 'Есть посуда и кухонные принадлежности.', 'Удобное рассположение в центре Токио', 'Понорамный вид из окон'];
-var advertisingPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 // удаляем класс на блоке с картой(временное решение)
 document.querySelector('.map').classList.remove('map--faded');
@@ -37,35 +42,31 @@ function shuffle(anyArray) { // перемешиваем массив
 }
 
 function getArrayRandomLength(anyArray) { // массив случайной длинны заполненный случайными данными
-  var arrayRandomLength = [];
   var shuffleArray = shuffle(anyArray);
-  for (var i = 0; i <= getRandomInteger(0, anyArray.length); i++) {
-    arrayRandomLength[i] = (shuffleArray[i]);
-  }
-  return arrayRandomLength;
+  return shuffleArray.slice(0, getRandomInteger(0, shuffleArray.length));
 }
 
 // основные функции
 function generateAdvertisings(advertisingsQuantity) { // создаем массив обявлений
   for (var i = 0; i < advertisingsQuantity; i++) {
     var locationX = getRandomInteger(0, mapPinsBlock.offsetWidth);
-    var locationY = getRandomInteger(130, 630);
+    var locationY = getRandomInteger(minLocationY, maxLocationY);
     advertisings[i] = {
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
-        title: getArrayRandomElement(advertisingTitles),
+        title: getArrayRandomElement(ADVERTISING_TITLES),
         address: locationX + ',' + locationY,
-        price: getRandomInteger(1, 100) * 100,
-        type: getArrayRandomElement(advertisingTypes),
-        rooms: getRandomInteger(1, 100),
-        guests: getRandomInteger(0, 3),
-        checkin: getArrayRandomElement(advertisingChecks),
-        checkout: getArrayRandomElement(advertisingChecks),
-        features: getArrayRandomLength(advertisingFeatures),
-        description: getArrayRandomLength(advertisingDescriptions),
-        photos: getArrayRandomLength(advertisingPhotos)
+        price: getRandomInteger(1, maxPriceMultiplier) * priceMultiplier,
+        type: getArrayRandomElement(ADVERTISING_TYPES),
+        rooms: getRandomInteger(1, maxRooms),
+        guests: getRandomInteger(0, maxGuests),
+        checkin: getArrayRandomElement(ADVERTISING_CHECKS),
+        checkout: getArrayRandomElement(ADVERTISING_CHECKS),
+        features: getArrayRandomLength(ADVERTISING_FEATURES),
+        description: getArrayRandomLength(ADVERTISING_DESCRIPTIONS),
+        photos: getArrayRandomLength(ADVERTISING_PHOTOS)
       },
       location: {
         x: locationX,
@@ -73,16 +74,24 @@ function generateAdvertisings(advertisingsQuantity) { // создаем масс
       }
     };
   }
+  return advertisings;
 }
 
 function generateAdvertisingPins(advertisingsQuantity) { // создаем пины для обявлений
   var fragment = document.createDocumentFragment();
+  var mapPinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+  var renderPin = function (advertisings) {
+    var mapPin = mapPinTemplate.cloneNode(true);
+
+    mapPin.style.cssText = 'left: ' + (advertisings.location.x - mapPinTemplate.offsetWidth / 2) + 'px; top: ' + (advertisings.location.y + mapPinTemplate.offsetHeight) + 'px;';
+    mapPin.innerHTML = '<img src="' + advertisings.author.avatar + '" width="40" height="40" draggable="false" alt="' + advertisings.offer.title + '">';
+
+    return mapPin;
+  };
   for (var i = 0; i < advertisingsQuantity; i++) {
-    var newElement = document.createElement('button');
-    newElement.className = 'map__pin';
-    newElement.style.cssText = 'left: ' + (advertisings[i].location.x + mapPin.offsetWidth / 2) + 'px; top: ' + (advertisings[i].location.y + mapPin.offsetHeight) + 'px;';
-    newElement.innerHTML = '<img src="' + advertisings[i].author.avatar + '" width="40" height="40" draggable="false" alt="' + advertisings[i].offer.title + '">';
-    fragment.appendChild(newElement);
+    fragment.appendChild(renderPin(advertisings[i]));
   }
   mapPinsBlock.appendChild(fragment);
 }
